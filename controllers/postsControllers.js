63764f15a45851be6f030ec542c51d5b53ccc272
giustoantonadev <giustoantonadev@gmail.com>
2026-04-07 12:04:32 +0200

    implement logic to the controller functions

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,11 +1,38 @@
-exports.index = (req,res) => {
-    res.send('INDEX: lista dei post')
+const posts = require('../data/posts')
+
+exports.index = (req, res) => {
+    let result = posts;
+
+    if (req.query.tag) {
+        result = posts.filter(post => {
+            return Array.isArray(post.tags) && post.tags.includes(req.query.tag)
+        })
+    }
+    res.json(result)
 };
 
-exports.show = (req,res) => {
-    res.send('SHOW: singolo post')
+exports.show = (req, res) => {
+    const id = Number(req.params.id);
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        return res.status(404).json({ error: `Post ${id} non trovato` })
+    }
+    res.json(post)
 };
 
 exports.destroy = (req, res) => {
-    res.send('DESTROY: elimina post')
+    const id = Number(req.params.id);
+    const index = posts.findIndex(p => p.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ error: `Post ${id} non trovato` })
+    }
+
+    posts.splice(index, 1);
+
+    console.log('Posts aggiornati:', posts);
+
+
+    res.status(204).end()
 };
